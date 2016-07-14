f8a8c73cd629ac1a2351d57ca058396a5cb9d1e1
oldtimers <oldtimers.me@gmail.com>
2016-07-14 17:08:23 -0400

    init

---
+++ module/Application/src/Application/Service/Service.php
@@ -0,0 +1,81 @@
+<?php 
+
+class Service {
+	public function getRemoteCategories() {
+		return array(
+				array('externalId' => 'unique id from source', 'categoryName' => 'name from source'),
+				array('externalId' => 'unique id from source', 'categoryName' => 'name from source'),
+		);
+	}
+	
+	public function getRemoteTraits() {
+	
+		return array(
+				array("externalId" => 1, "traitName" => 'delivery'),
+				array("externalId" => 2, "traitName" => 'take out'),
+				array("externalId" => 3, "traitName" => 'catering'),
+				array("externalId" => 4, "traitName" => 'vegetarian'),
+				array("externalId" => 5, "traitName" => 'vegan'),
+				array("externalId" => 6, "traitName" => 'gluten'),
+				//include here cousine types U fetch - if posisble fetch remote id if not assign by own and do hard code
+		);
+	}
+	
+	public function getRemoteData($offset = 0, $count = 100) {
+		return array(
+				'total' => 'count of all records',
+				'records' => array(
+						array(
+								'externalId' => '',
+								'longitude' => '',
+								'latitude' => '',
+								'addressState' => '',
+								'addressCounty' => '',
+								'addressCity' => '',
+								'name' => '',
+								'imageUrl' => '',
+								'categories' => array(), //matching ids from getRemoteCategories
+								'traits' => array(), //matching ids from getRemoteTraits
+								'customerRating' => '',
+								'priceRating' => '',
+								'websiteUrl' => '',
+		    ‚internationalPhoneNumber’ => '',
+                    'hoursOfOperation' => array(
+                        array(
+                            "open" => array(
+                                "day" => '0',
+                                "time" => 'hhmm'
+                            ),
+                            "close" => array(
+                                "day" => '0',
+                                "time" => 'hhmm'
+                            )
+                        ),
+                        array(
+                            "open" => array(
+                                "day" => '1',
+                                "time" => 'hhmm'
+                            ),
+                            "close" => array(
+                                "day" => '1',
+                                "time" => 'hhmm'
+                            )
+                        ),
+                        array(
+                            "open" => array(
+                                "day" => '2',
+                                "time" => 'hhmm'
+                            ),
+                            "close" => array(
+                                "day" => '2',
+                                "time" => 'hhmm'
+                            )
+                        )
+                    ),
+                )
+            )
+        );
+    }
+	
+	
+}
